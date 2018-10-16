c7626ff70145a1832d7a492df6802872f19ba343
Bhoomi Gohil <bhoomi_gohil@hotmail.com>
2018-10-16 14:30:04 +0530

    Apply formation and write Comments

--- Js.js
+++ Js.js
@@ -1,25 +1,25 @@
-var pic = 15 , number = 12 ;
-var start = 1, end = 12 ;
-var repeation = pic / number ;
+var pic = 15; // Total number of images in database.
+var number = 12; // Only Display 12 images in webpage.
+var start = 1, end = 12;
+var repeation = pic / number; // Total images divided by 12.
 
-var main = document.querySelector(".main");
-var glassScreen = document.querySelector(".glassScreen");
-var display = document.querySelector(".display");
+var main = document.querySelector(".main"); // Select main box for clear old appendChild.
+var glassScreen = document.querySelector(".glassScreen"); // Select glass class for do operation.
+var display = document.querySelector(".display"); // Select display class for display big image.
 
-function clearsmallimage(main) {
+function clearsmallimage(main) { // Clear firstchild of main class.
     while (main.hasChildNodes()) {
         main.removeChild(main.firstChild);
     }
 }
 
-function clearbigimage(display) {
-    while(display.hasChildNodes()){
-      display.removeChild(display.firstChild);
+function clearbigimage(display) { // Clear firstchild of display class.
+    while (display.hasChildNodes()) {
+        display.removeChild(display.firstChild);
     }
 }
 
-function displaySmallImage(start , end)
-{
+function displaySmallImage(start, end) {  // Display small images on webpage.
     for (i = start; i <= end; i++) {
         var div = document.createElement("div");
         div.className = "div-box" + [i];
@@ -28,50 +28,46 @@ function displaySmallImage(start , end)
 
         var img = document.createElement("img");
         img.value = i;
-        img.src = "Images/" + [i] + ".JPG" ;
+        img.src = "Images/" + [i] + ".JPG";
         img.setAttribute("class", "image");
-        img.onclick = function(){ imageclick(this.value) };
+        img.onclick = function () { imageclick(this.value) };
         document.querySelector(".div-box" + [i]).appendChild(img);
     }
 }
 
-displaySmallImage(start , end);
+displaySmallImage(start, end); // Display small images by default.
 
-function numberButtonClick(text)
-{
+function numberButtonClick(text) {
     clearsmallimage(main);
     j = 1;
 
-    for(i = 1 ; i <= text ; i++)
-    {
-      start = j ;
-      j = j + 12;
-      end = number * text;
-      if(end > pic)
-      {
-        end = pic;
-      }
+    for (i = 1; i <= text; i++) {
+        start = j;
+        j = j + 12;
+        end = number * text;
+        if (end > pic) {
+            end = pic;
+        }
     }
 
-    displaySmallImage(start , end);
+    displaySmallImage(start, end); // Display small images.
 }
 
-function imageclick(value)
-{
-  clearbigimage(display);
+function imageclick(value) { // Click on small image to display particular image in big way.
+    clearbigimage(display);
 
-  glassScreen.style.display = "block";
-  display.style.display = "block";
+    glassScreen.style.display = "block";
+    display.style.display = "block";
 
-  var displayimg = document.createElement("img");
-  displayimg.src = "Images/" + [value] + ".JPG";
-  displayimg.alt = "Images/16.jpg";
-  displayimg.setAttribute("class","image");
-  displayimg.setAttribute("id","bigimage");
-  document.querySelector(".display").appendChild(displayimg);
+    var displayimg = document.createElement("img");
+    displayimg.src = "Images/" + [value] + ".JPG";
+    displayimg.alt = "Images/16.jpg";
+    displayimg.setAttribute("class", "image");
+    displayimg.setAttribute("id", "bigimage");
+    document.querySelector(".display").appendChild(displayimg);
 }
 
-for (i = 0; i <= repeation; i++) {
+for (i = 0; i <= repeation; i++) { // It is for display below number buttons to view number of images.
     var button = document.createElement("button");
     button.innerHTML = i + 1;
     button.className = "button";
@@ -79,8 +75,7 @@ for (i = 0; i <= repeation; i++) {
     document.querySelector(".button-div").appendChild(button);
 }
 
-function hide()
-{
-  glassScreen.style.display = "none";
-  display.style.display = "none";
+function hide() { // Hide big large box.
+    glassScreen.style.display = "none";
+    display.style.display = "none";
 }
