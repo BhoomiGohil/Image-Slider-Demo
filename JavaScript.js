b2e8be5686bec3bd3b4ad0c60cfffa5321d9bda6
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2024-10-22 15:21:40 -0400

    Updated css and some js code.

--- JavaScript.js
+++ JavaScript.js
@@ -1,166 +1,159 @@
-// Collect all images from the HTML.
-var dbimg = document.querySelectorAll(".dbimg");
+// Array of image objects with URLs and IDs.
+var images = [
+  { url: "./Images/1.jpg", id: 1 },
+  { url: "./Images/2.jpg", id: 2 },
+  { url: "./Images/3.jpg", id: 3 },
+  { url: "./Images/4.jpg", id: 4 },
+  { url: "./Images/5.jpg", id: 5 },
+  { url: "./Images/6.jpg", id: 6 },
+  { url: "./Images/7.jpg", id: 7 },
+  { url: "./Images/8.jpg", id: 8 },
+  { url: "./Images/9.jpg", id: 9 },
+  { url: "./Images/10.jpg", id: 10 },
+  { url: "./Images/11.jpg", id: 11 },
+  { url: "./Images/12.jpg", id: 12 },
+  { url: "./Images/13.jpg", id: 13 },
+  { url: "./Images/14.jpg", id: 14 },
+  { url: "./Images/15.jpg", id: 15 },
+];
 
-// Store the total number of images in the database.
-var pic = dbimg.length;
+// Select necessary HTML elements.
+var dbimg = images; // Reference to the images array.
+var imageContainer = document.querySelector(".imageContainer"); // Container for small images.
+var screenWrapper = document.querySelector(".screen-wrapper"); // Overlay wrapper.
+var display = document.querySelector(".display"); // Container for large image display.
+var glassScreen = document.querySelector(".glassScreen"); // Overlay screen.
+var displaybutton = document.querySelector(".display-button"); // Arrow button container.
 
-// Set the number of images to display on the webpage.
-var number = 12;
+var pic = dbimg.length; // Total number of images.
+var number = 12; // Number of images per set.
+var start = 0,
+  end = number - 1; // Range for the initial set.
+var repetition = pic / number; // Total sets based on the number of images.
 
-// Define the range for the first set of images to display.
-var start = 1,
-  end = number;
-
-// Calculate how many sets of 12 images are possible.
-var repetition = pic / number;
-
-// Select the imageContainer container for displaying small images.
-var imageContainer = document.querySelector(".imageContainer");
-
-// Select the display container for showing large images.
-var display = document.querySelector(".display");
-
-// Select the overlay element for displaying large images.
-var glassScreen = document.querySelector(".glassScreen");
-
-// Select the arrow button container for hiding/showing arrow buttons.
-var displaybutton = document.querySelector(".display-button");
-
-// Function to clear all child elements from the 'imageContainer' container.
-function clearsmallimage(imageContainer) {
-  while (imageContainer.hasChildNodes()) {
-    imageContainer.removeChild(imageContainer.firstChild);
-  }
-}
-
-// Function to clear all child elements from the 'display' container.
-function clearbigimage(display) {
+// Clear all elements from the display container.
+function clearBigImage(display) {
   while (display.hasChildNodes()) {
     display.removeChild(display.firstChild);
   }
 }
 
-// Function to display a range of small images on the webpage.
+// Display a large version of the clicked image.
+function imageClick(value) {
+  clearBigImage(display); // Remove any previous large image.
+  screenWrapper.style.display = "flex"; // Show the overlay.
+
+  var displayimg = document.createElement("img"); // Create large image element.
+  displayimg.setAttribute("value", value);
+  displayimg.src = images[value].url;
+  displayimg.alt = "Big Images that display on screen";
+  displayimg.setAttribute("id", "bigimage");
+
+  display.appendChild(displayimg); // Add the large image to display container.
+}
+
+// Display a set of small images.
 function displaySmallImage(start, end) {
-  for (i = start; i <= end; i++) {
-    // Create a new div container for each image.
-    var div = document.createElement("div");
-    div.className = "div-box" + [i];
-    div.setAttribute("id", "division");
-    document.querySelector(".imageContainer").appendChild(div);
+  for (let i = start; i <= end; i++) {
+    var div = document.createElement("div"); // Create a div for each image.
+    div.setAttribute("class", "smallImageEl");
+    div.setAttribute("id", "div-box" + i);
+    imageContainer.appendChild(div);
 
-    // Create an image element and set its properties.
-    var img = document.createElement("img");
-    img.value = i;
-
-    // Set the source of the image based on the corresponding dbimg.
-    if (img.value === i) {
-      img.src = dbimg[i - 1].src;
-    }
-
+    var img = document.createElement("img"); // Create an image element.
+    img.setAttribute("value", i);
     img.setAttribute("class", "image");
+    img.src = images[i].url;
 
-    // Set an onclick event to display the image in larger view when clicked.
-    img.onclick = function () {
-      imageclick(this.value);
+    img.onclick = function (event) {
+      imageClick(event.target.getAttribute("value")); // Show large image on click.
     };
 
-    // Append the image to the corresponding div container.
-    document.querySelector(".div-box" + [i]).appendChild(img);
+    div.appendChild(img); // Add the image to the div.
   }
 }
 
 // Display the first set of images by default.
 displaySmallImage(start, end);
 
-// Function to handle clicks on number buttons to display specific sets of images.
-function numberButtonClick(text) {
-  clearsmallimage(imageContainer); // Clear old images.
+// Clear all small images from the image container.
+function clearSmallImage(imageContainer) {
+  while (imageContainer.hasChildNodes()) {
+    imageContainer.removeChild(imageContainer.firstChild);
+  }
+}
 
-  j = 1; // Start index for the new set of images.
+// Display specific sets of images based on button clicks.
+function numberButtonClick(id) {
+  clearSmallImage(imageContainer); // Clear previous images.
 
-  for (i = 1; i <= text; i++) {
-    start = j;
-    j = j + number;
-    end = number * text;
+  var index = 0; // Start index for new set.
 
-    // Ensure 'end' does not exceed the total number of images.
-    if (end > pic) {
-      end = pic;
+  for (let i = 1; i <= id; i++) {
+    start = index;
+    index += number;
+    end = number * id - 1;
+
+    if (end >= pic) {
+      end = pic - 1; // Ensure 'end' doesn't exceed total images.
     }
   }
 
-  // Display the new set of images.
-  displaySmallImage(start, end);
+  displaySmallImage(start, end); // Display the new set.
 }
 
-// Create buttons to navigate between different sets of images.
-for (i = 0; i <= repetition; i++) {
+// Create buttons for navigating between image sets.
+for (let i = 0; i <= repetition; i++) {
   var button = document.createElement("button");
-  button.innerHTML = i + 1; // Button label.
+  button.setAttribute("id", i);
+  button.innerHTML = i + 1; // Set button label.
   button.className = "button";
 
-  // Set an onclick event to display the corresponding set of images.
   button.onclick = function () {
-    numberButtonClick(this.innerHTML);
+    numberButtonClick(parseInt(this.id) + 1); // Display corresponding image set.
   };
 
-  // Append the button to the button container.
-  document.querySelector(".button-div").appendChild(button);
+  document.querySelector(".button-div").appendChild(button); // Add button to container.
 }
 
-// Function to display a clicked image in larger view.
-function imageclick(value) {
-  clearbigimage(display); // Clear any previously displayed large image.
-
-  // Show the overlay and the display elements.
-  glassScreen.style.display = "block";
-  display.style.display = "block";
-  displaybutton.style.display = "block";
-
-  // Create an image element for the large view and set its properties.
-  var displayimg = document.createElement("img");
-  displayimg.value = value;
-  displayimg.src = dbimg[value - 1].src;
-  displayimg.alt = "Images/16.jpg"; // Alt text for the image.
-  displayimg.setAttribute("class", "image");
-  displayimg.setAttribute("id", "bigimage");
-
-  // Append the large image to the display container.
-  document.querySelector(".display").appendChild(displayimg);
+// Update the large image view in a loop.
+function imageloop(value) {
+  var bigimage = document.querySelector("#bigimage");
+  for (let i = start; i <= pic; i++) {
+    if (value === i) {
+      bigimage.src = images[i].url; // Update large image source.
+      bigimage.setAttribute("value", i);
+    }
+  }
 }
 
-// Function to handle left and right arrow navigation on large images.
+// Navigate between large images using arrows.
 function arrow(value) {
-  var bigimage = document.querySelector("#bigimage"); // Select the current large image.
+  var bigimage = document.querySelector("#bigimage"); // Current large image.
+  var smallimage = document.querySelectorAll(".image"); // All small images.
+
+  var bigImageValue = parseInt(bigimage.getAttribute("value"));
+  var smallImageFirstValue = parseInt(smallimage[0].getAttribute("value"));
+  var smallImageLastValue = parseInt(
+    smallimage[smallimage.length - 1].getAttribute("value")
+  );
 
   if (value === "left") {
-    bigimage.value = bigimage.value - 1; // Move to the previous image.
-    if (bigimage.value === 0) {
-      bigimage.value = pic; // Loop back to the last image if at the start.
+    bigImageValue -= 1; // Move left.
+    if (bigImageValue < smallImageFirstValue) {
+      bigImageValue = smallImageLastValue; // Loop back to last image.
     }
-    imageloop(bigimage.value); // Update the displayed image.
   } else {
-    bigimage.value = bigimage.value + 1; // Move to the next image.
-    if (bigimage.value === pic + 1) {
-      bigimage.value = start; // Loop back to the first image if at the end.
-    }
-    imageloop(bigimage.value); // Update the displayed image.
-  }
-}
-
-// Function to update the large image based on the current value.
-function imageloop(value) {
-  for (i = start; i <= pic; i++) {
-    if (value === i) {
-      bigimage.src = dbimg[i - 1].src; // Set the source of the large image.
+    bigImageValue += 1; // Move right.
+    if (bigImageValue > smallImageLastValue) {
+      bigImageValue = smallImageFirstValue; // Loop back to first image.
     }
   }
+
+  imageloop(bigImageValue); // Update large image.
 }
 
-// Function to hide the large image display and overlay.
+// Hide the large image overlay.
 function hide() {
-  glassScreen.style.display = "none";
-  display.style.display = "none";
-  displaybutton.style.display = "none";
+  screenWrapper.style.display = "none"; // Hide overlay.
 }
